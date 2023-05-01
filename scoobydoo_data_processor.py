99915cc8d25ff261a188de7f3bb4dc4ea500136c
nawodyaishan <nawodyain@gmail.com>
2023-05-02 00:27:06 +0530

    feat: add Remove season and episode indicators

--- scoobydoo_data_processor.py
+++ scoobydoo_data_processor.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import re
 
 import pandas as pd
 
@@ -7,14 +8,18 @@ import pandas as pd
 # Add ASCII text logo and main menu
 def display_logo():
     logo = r"""
-
-  _________                  ___.          ________          __           ___________           .__   
- /   _____/ ____  ____   ____\_ |__ ___.__.\______ \ _____ _/  |______    \__    ___/___   ____ |  |  
- \_____  \_/ ___\/  _ \ /  _ \| __ <   |  | |    |  \\__  \\   __\__  \     |    | /  _ \ /  _ \|  |  
- /        \  \__(  <_> |  <_> ) \_\ \___  | |    `   \/ __ \|  |  / __ \_   |    |(  <_> |  <_> )  |__
-/_______  /\___  >____/ \____/|___  / ____|/_______  (____  /__| (____  /   |____| \____/ \____/|____/
-        \/     \/                 \/\/             \/     \/          \/                              
-
+  ______                                 __                 
+ /      \                               |  \                
+|  $$$$$$\  _______   ______    ______  | $$____   __    __ 
+| $$___\$$ /       \ /      \  /      \ | $$    \ |  \  |  \
+ \$$    \ |  $$$$$$$|  $$$$$$\|  $$$$$$\| $$$$$$$\| $$  | $$
+ _\$$$$$$\| $$      | $$  | $$| $$  | $$| $$  | $$| $$  | $$
+|  \__| $$| $$_____ | $$__/ $$| $$__/ $$| $$__/ $$| $$__/ $$
+ \$$    $$ \$$     \ \$$    $$ \$$    $$| $$    $$ \$$    $$
+  \$$$$$$   \$$$$$$$  \$$$$$$   \$$$$$$  \$$$$$$$  _\$$$$$$$
+                                                  |  \__| $$
+                                                   \$$    $$
+                                                    \$$$$$$ 
     """
     print(logo)
 
@@ -32,6 +37,11 @@ def main_menu():
 # Create a function to handle user input
 def get_user_choice():
     return input("Please select an option (1-3): ")
+
+
+# Function to remove season and episode indicators from all columns and rows
+def remove_season_episode(text):
+    return re.sub(r'S\d{2}E\d{2}', '', text)
 
 
 # to split the text into chunks, ensuring that the split occurs at the end of a sentence, denoted by a period
@@ -53,7 +63,7 @@ def split_at_sentence(text, max_tokens):
     return chunks
 
 
-# Define the function to split the plots
+# Function to split a plot into smaller chunks based on the user-defined token limit
 def split_plot(plot, max_tokens):
     words = plot.split()
     num_parts = math.ceil(len(words) / max_tokens)
@@ -67,37 +77,55 @@ def split_plot(plot, max_tokens):
     return chunks
 
 
-# Process the DataFrame
-def process_data(df, max_tokens, remove_nbsp):
+# Function to process the data by either splitting long plots or removing specific characters
+# Updated process_data function with 'remove_se' option
+def process_data(df, max_tokens, action, remove_char=None):
     new_rows = []
 
     for index, row in df.iterrows():
         plot_source_name = row['Plot Source Name']
         plot = row['Plot']
 
-        if remove_nbsp:
-            plot = plot.replace('\xa0', ' ')
+        if action == 'split':
+            if len(plot.split()) > max_tokens:
+                chunks = split_plot(plot, max_tokens)
+                for idx, chunk in enumerate(chunks):
+                    new_row = {
+                        'Plot Source Name': f"{plot_source_name} - part {idx + 1}",
+                        'Plot': chunk
+                    }
+                    new_rows.append(new_row)
+            else:
+                new_rows.append(row)
+        elif action == 'remove_char':
+            if remove_char:
+                plot = plot.replace(remove_char, '')
+            else:
+                plot = plot.replace('\xa0', '')
+            new_row = {
+                'Plot Source Name': plot_source_name,
+                'Plot': plot
+            }
+            new_rows.append(new_row)
+        elif action == 'remove_se':
+            plot_source_name = remove_season_episode(plot_source_name)
+            plot = remove_season_episode(plot)
 
-        if len(plot.split()) > max_tokens:
-            chunks = split_plot(plot, max_tokens)
-
-            for i, chunk in enumerate(chunks, 1):
-                new_rows.append({
-                    'Plot Source Name': f"{plot_source_name} - part {i}",
-                    'Plot': f"part {i} of {plot_source_name} : {chunk}"
-                })
-        else:
-            new_rows.append({'Plot Source Name': plot_source_name, 'Plot': f"{plot_source_name}: {plot}"})
+            new_row = {
+                'Plot Source Name': plot_source_name,
+                'Plot': plot
+            }
+            new_rows.append(new_row)
 
     return pd.DataFrame(new_rows)
 
 
-# Read the input CSV file
+# Function to read a CSV file using pandas and return a DataFrame
 def read_csv(file_path):
     return pd.read_csv(file_path)
 
 
-# Write the output CSV file
+# Function to write a DataFrame to a CSV file using pandas
 def write_csv(df, file_path):
     df.to_csv(file_path, index=False)
 
@@ -126,31 +154,56 @@ def handle_action(choice, args):
         return False
 
 
-# main function
-def main(args):
-    display_logo()
-    main_menu()
-    choice = get_user_choice()
-    valid_choice = handle_action(choice, args)
+# Main function for handling user inputs and calling appropriate functions
+def main():
+    while True:
+        display_logo()
+        print("ScoobyDoo Data Processor")
+        print("-------------------------")
+        print("1. Split long plots")
+        print("2. Remove specific characters")
+        print("3. Exit")
+        choice = input("Please select an option (1-3): ")
 
-    if valid_choice:
-        if args.input is None or args.output is None:
-            print("Please provide input and output file paths.")
-            return
+        if choice == '1':
+            input_file = input("Enter the Input CSV file path: ")
+            output_file = input("Enter the Output CSV file path: ")
+            max_tokens = int(input("Enter the maximum tokens per plot chunk (default: 1000): "))
+            df = read_csv(input_file)
+            result = process_data(df, max_tokens, 'split')
+            write_csv(result, output_file)
+            print("Splitting process completed!")
+        elif choice == '2':
+            print("1. Remove non-breaking spaces (NBSP)")
+            print("2. Remove custom character")
+            print("3. Remove season and episode indicators")
+            sub_choice = input("Please select an option (1-3): ")
+            input_file = input("Enter the Input CSV file path: ")
+            output_file = input("Enter the Output CSV file path: ")
+            df = read_csv(input_file)
 
-        if args.action == "splitter":
-            df = read_csv(args.input)
-            processed_df = process_data(df, args.tokens, remove_nbsp=False)
-            write_csv(processed_df, args.output)
-        elif args.action == "remove_nbsp":
-            df = read_csv(args.input)
-            processed_df = process_data(df, args.tokens, remove_nbsp=True)
-            write_csv(processed_df, args.output)
+            if sub_choice == '1':
+                result = process_data(df, None, 'remove_char')
+                print("Non-breaking spaces removed!")
+            elif sub_choice == '2':
+                custom_char = input("Enter the custom character to remove: ")
+                result = process_data(df, None, 'remove_char', custom_char)
+                print(f"Custom character '{custom_char}' removed!")
+            elif sub_choice == '3':
+                result = process_data(df, None, 'remove_se')
+                print("Season and episode indicators removed!")
+            else:
+                print("Invalid option. Please try again.")
+                continue
+            write_csv(result, output_file)
+        elif choice == '3':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option. Please try again.")
+            continue
 
 
 # the conditional block
-if __name__ == '__main__':
-    args = parse_args()
-
-    while True:
-        main(args)
+if __name__ == "__main__":
+    main()
